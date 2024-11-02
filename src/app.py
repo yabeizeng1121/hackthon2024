9320a7277cd098258c38353276c56e2415d21505
Jeremy Tan <jmt132@duke.edu>
2024-11-02 18:44:08 -0400

    change port config

--- src/app.py
+++ src/app.py
@@ -219,4 +219,4 @@ def cleanup():
 atexit.register(cleanup)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
